f875b1c1198747900f3e88a9e9a58ae506bc604b
Alicha122 <6631501122@lamduan.mfu.ac.th>
2025-08-28 15:28:55 +0700

    add login

--- bin/dart_project1.dart
+++ bin/dart_project1.dart
@@ -1,9 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+const String baseUrl = "http://localhost:3000";
+String? loggedInUserId;
 void main() {
 
 }
 
 void login() {
+  Future<void> login() async {
+  print("===== Login =====");
 
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+
+  if (username == null || password == null || username.isEmpty || password.isEmpty) {
+    print("Incomplete input");
+    return;
+  }
+
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('$baseUrl/login');
+  final response = await http.post(url, body: body);
+
+  if (response.statusCode == 200) {
+    final result = jsonDecode(response.body);
+    print(" ${result['message']}");
+    loggedInUserId = result['userId'].toString();
+    await showMenu();
+  } else {
+    final result = jsonDecode(response.body);
+    print(" ${result['message']}");
+  }
+}
 }
 
 void showmenu() {
